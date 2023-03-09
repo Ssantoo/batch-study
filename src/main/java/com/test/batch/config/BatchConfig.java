58d84ed0a645a713429aaae41d50f184e7d85e00
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-03-09 19:26:41 +0900

    junit 준비

---
+++ src/main/java/com/test/batch/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.test.batch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+}
